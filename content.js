5858a1395f0e80a08580e754aa3c4dd615d8b182
Miles Fertel <milesfertel@college.harvard.edu>
2017-09-16 17:14:09 -0400

    Handle amazon checkout page scraping

--- content.js
+++ content.js
@@ -1,3 +1,14 @@
 function getPrice() {
   // Returns negative number if not a transaction page. Otherwise gets price.
-};
+  var elts = document.getElementsByClassName("grand-total-price");
+  if (elts.length) {
+    var total = elts[0].innerHTML;
+    total = Number(total.replace(/[^0-9\.-]+/g,""));
+    return parseFloat(total);
+  }
+  return -1;
+}
+
+function getRoundUp(total) {
+  return parseFloat((Math.ceil(total) - total).toFixed(2));
+}
