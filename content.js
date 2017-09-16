599aca434f49a74c5a11c1042cb01b4c845f63d6
LisaLudique <lisacarolynlu@gmail.com>
2017-09-16 16:55:00 -0400

    Edit confirm message

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ var price = getPrice();
 if (price > 0) {
   var roundUp = getRoundUp(price);
 }
-if (confirm("You can round up and donate " + roundUp.toString())) {
+if (confirm("You can round up and donate " + roundUp.toString() + ". Press OK to donate, or cancel to exit.")) {
   // User pressed "OK" and decided to pay.
 } else {
   // User didn't want to pay.
