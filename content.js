517bd2a5a2bbc7ffe45d59e31ea5e7fefd27de3a
LisaLudique <lisacarolynlu@gmail.com>
2017-09-16 16:49:58 -0400

    Add popup logic

--- content.js
+++ content.js
@@ -1,3 +1,17 @@
 function getPrice() {
   // Returns negative number if not a transaction page. Otherwise gets price.
 };
+
+function getRoundUp () {
+  // Re
+}
+
+var price = getPrice();
+if (price > 0) {
+  var roundUp = getRoundUp(price);
+}
+if (confirm("You can round up and donate " + roundUp.toString())) {
+  // User pressed "OK" and decided to pay.
+} else {
+  // User didn't want to pay.
+}
